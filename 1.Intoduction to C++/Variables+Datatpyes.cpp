95637ad4a45dc85e8c39f37893589b7a96ca098c
Kanhaiya Kumar <kanhaiyak0104@gmail.com.com>
2024-09-10 23:15:32 +0530

    Refactor C++ code files and add First program and Variables+Datatypes files

---
+++ 1.Intoduction to C++/Variables+Datatpyes.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    //Variables Define
+
+    // int age = 25;
+    // cout << "My age is " << age << endl;
+
+    //Decalaration
+    // int age;
+    // cout <<age;
+
+    //Defination
+    // int age = 21;
+    // cout << "My age is " << age << endl;
+
+
+    // Manupulation and Update
+    // age = 22;
+    // cout << age;
+
+
+
+
+    //........................DATA TYPES.............................
+
+     //int
+     int count = 4;
+
+     //float
+     float share = 3.14;
+
+     //char
+     char alphabate = 'A';
+     
+     //dobule
+     double weight = 55.99886;
+
+
+     //bool
+
+     bool isMan = 1 or true;
+     bool isMen = 0 or false;
+
+
+     cout << count << endl;
+        cout << share << endl;
+        cout << alphabate << endl;
+        cout << weight << endl;
+        cout << isMan << endl;
+        cout << isMen << endl;
+
+
+    return 0;
+}
