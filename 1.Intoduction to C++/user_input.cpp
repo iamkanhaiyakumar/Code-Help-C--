9d2b86a0fcbb19dfa5f0f20e58fe1b8df0318dd1
Kanhaiya Kumar <kanhaiyak0104@gmail.com.com>
2024-09-29 21:23:39 +0530

    Add user input functionality to C++ code

---
+++ 1.Intoduction to C++/user_input.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    //Variables decalaration
+    int marks;
+    cout << "Enter your marks: ";
+
+    //input from user
+    cin >> marks;
+    cout<<"Your marks are: "<<marks<<endl;
+
+    return 0;
+}
