a7a3552ed67fc9bccde86dd06c8937b14839cc74
Kanhaiya Kumar <kanhaiyak0104@gmail.com.com>
2024-11-30 12:03:18 +0530

    Enable error squiggles in VSCode settings and update function comments in Bitwise operations example

--- 2.Intoduction to C++/Pattern/Bitwise_op_and_functions/function.cpp
+++ 2.Intoduction to C++/Pattern/Bitwise_op_and_functions/function.cpp
@@ -1,26 +1,47 @@
 #include<iostream>
 using namespace std;
  
-// void myNmae(){
+// void myNmae() # return nothing 
+// {
 //     cout << "Kanhaiya Kumar" << endl;
 // }
 
-int slove(int a, int b)
-{
-    int sum = a + b;
-    return sum;
+// int slove(int a, int b)
+// {
+//     int sum = a + b;
+//     return sum;
+// }
+
+
+// void print2katable(){
+
+//*****parameter*****//
+void printNkatable(int n){
+
+
+    for (int i=1; i<=10; i++){
+        // cout<<2*i <<endl;
+        cout<<n*i << endl;
+    }
 }
+
+
 
 
 int main()
 {
     // myNmae();
     
-    int a  = 5;
-    int b = 10; 
+    // int a  = 5;
+    // int b = 10; 
 
-    int sum1 = slove(a,b);
-    cout <<  sum1 << endl;
+    // int sum1 = slove(a,b);
+    // cout <<  sum1 << endl;
 
+    // print2katable();
+
+    printNkatable(5);
+
+    
     return 0;
 }
