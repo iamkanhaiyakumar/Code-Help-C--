eee6cdc1a5dd4ca3a28315a753ce4e7da3335019
Kanhaiya Kumar <kanhaiyak0104@gmail.com>
2025-04-25 22:10:32 +0530

    class 3

--- Arrays/3rd class/1.cpp
+++ Arrays/3rd class/1.cpp
@@ -16,10 +16,10 @@ int main(){
     //     {5, 6}
     // };
 
-    int arr[3][3] = 
-        {1, 2, 3, 
-         4, 5, 6,
-         7, 8, 9}; // this is also a valid way to initialize a 2D array
+    // int arr[3][3] = 
+    //     {1, 2, 3, 
+    //      4, 5, 6,
+    //      7, 8, 9}; // this is also a valid way to initialize a 2D array
 
     // this will initialize all elements to 0
     // int arr[3][2] = {}; 
@@ -28,8 +28,8 @@ int main(){
     // cout<<arr[1][0]<<endl;
 
     //traversing entire 2D array
-    int rowSize = 3;
-    int colSize = 3;
+    // int rowSize = 3;
+    // int colSize = 3;
 
     //same as the pattern of 1D array
     
@@ -41,6 +41,8 @@ int main(){
     //     }
     //     cout<<endl;
     // }
+
+
     // column-wise traversing
     // for(int colIndex =0; colIndex<colSize; colIndex++) {
     //     for(int rowIndex =0; rowIndex<rowSize; rowIndex++) {
@@ -49,14 +51,51 @@ int main(){
     //     cout<<endl;
     // }
 
-    //diagonal traversing
-    for(int i= 0; i<rowSize; i++){
-        cout<<arr[i][i]<<" "<<endl;
+    // //diagonal traversing
+    // for(int i= 0; i<rowSize; i++){
+    //     cout<<arr[i][i]<<" "<<endl;
+    // }
+    // cout<<endl;
+    // //anti-diagonal traversing
+    // for(int i= 0; i<rowSize; i++){
+    //     cout<<arr[i][rowSize-i-1]<<" "<<endl;
+    // }
+
+    //taking input for 2D array
+    int arr[2][3]; // Declare a 2D array of size 3x3
+    int rowSize = 3;
+    int colSize = 4;
+
+    //taking input
+
+    for (int i = 0; i < rowSize; i++) {
+        for (int j = 0; j < colSize; j++) {
+            cout << "Enter element at position [" << i << ", " << j << "]: ";
+            cin >> arr[i][j];
+        }
     }
-    cout<<endl;
-    //anti-diagonal traversing
-    for(int i= 0; i<rowSize; i++){
-        cout<<arr[i][rowSize-i-1]<<" "<<endl;
+
+    cout << "The 2D array is: " << endl;
+
+  //row-wise 
+
+    // for(int rowIndex =0; rowIndex<rowSize; rowIndex++) {
+    //     for(int colIndex =0; colIndex<colSize; colIndex++) {
+    //         cout<<arr[rowIndex][colIndex]<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    // column-wise traversing
+    for(int colIndex =0; colIndex<colSize; colIndex++) {
+        for(int rowIndex =0; rowIndex<rowSize; rowIndex++) {
+            cout<<arr[rowIndex][colIndex]<<" ";
+        }
+        cout<<endl;
     }
+
+
+
+
     return 0;
 }
