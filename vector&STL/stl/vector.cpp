cd86ce5b0a05db03216a4a1c1b15b0884a2a32b4
Kanhaiya Kumar <kanhaiyak0104@gmail.com>
2025-05-02 18:43:15 +0530

    vector

--- vector&STL/stl/vector.cpp
+++ vector&STL/stl/vector.cpp
@@ -10,6 +10,15 @@ int main(){
     marks.push_back(20); 
     marks.push_back(30); 
     marks.push_back(40);
+    marks.push_back(50);
+    
+
+    cout<<marks[0]<<endl; // Access the first element
+    cout<<marks[1]<<endl; // Access the second element
+
+    marks[0] = 100; // Modify the first element
+    cout<<marks[0]<<endl; // Access the first element again
+
 
     cout<<"size of vector: "<<marks.size()<<endl;
     //remove the vector elements
@@ -18,6 +27,29 @@ int main(){
     
     cout<< marks.front()<<endl; // Access the first element
     cout<< marks.back()<<endl; // Access the last element
+
+
+    if(marks.empty()==true){
+        cout<<"vector is empty"<<endl;
+    }else{
+        cout<<"vector is not empty"<<endl;
+    }
+
+    // cout<<marks[0]<<endl; 
+    cout<<marks.at(0)<<endl; 
+
+    
+    cout<<marks.capacity()<<endl; // Capacity of the vector
+    cout<<marks.size()<<endl; // Maximum size of the vector
+    cout<<marks.data()<<endl; // Address of the first element
+    cout<<marks.max_size()<<endl; // Maximum size of the vector
+
+
+    
+
+    marks.clear(); // Clear the vector
+    cout<<"size of vector: "<<marks.size()<<endl;
+
 
     return 0;
 }
