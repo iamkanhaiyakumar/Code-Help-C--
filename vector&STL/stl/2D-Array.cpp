9ca36adad054daa5142f17c8c468988db0128a49
Kanhaiya Kumar <kanhaiyak0104@gmail.com>
2025-05-03 17:45:10 +0530

    vector

--- vector&STL/stl/2D-Array.cpp
+++ vector&STL/stl/2D-Array.cpp
@@ -18,5 +18,18 @@ int main(){
     cout << "Size of each column: " << sizeof(arr[0][0]) << endl; // Size of each column
     
 
+    vector<vector<int> > brr(4);
+
+    brr[0]= vector<int>(4);
+    brr[1]= vector<int>(2);
+    brr[2]= vector<int>(5);
+    brr[3]= vector<int>(3);
+
+    int totalRowCount = brr.size(); // Number of rows
+    int totalColumnCount = brr[0].size(); // Number of columns
+
+    cout << "Number of rows: " << totalRowCount << endl;
+    cout << "Number of columns: " << totalColumnCount << endl;
+    
     return 0;
 }
---
+++ vector&STL/stl/llist.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<list> // Include the list header file
+#include<iterator> // Include the iterator header file
+#include<algorithm> // Include the algorithm header file
+using namespace std;
+
+int main(){
+    // Create a list of integers
+    list<int> mylist; 
+
+    //insert elements at the end of the list
+    mylist.push_back(10);
+    mylist.push_back(20);
+    mylist.push_back(30);
+
+    //insert elements at the beginning of the list
+    mylist.push_front(5);
+    mylist.push_front(1);
+
+    //insert elements at a specific position in the list
+    mylist.insert(mylist.begin(), 0); // Insert 0 at the beginning
+    mylist.insert(mylist.end(), 40); // Insert 40 at the end
+
+
+
+     // Display the elements of the list
+    cout << "Elements in the list: ";
+   
+    cout<<"size of list: "<<mylist.size()<<endl; // Size of the list
+
+    cout<<"front element of list: "<<mylist.front()<<endl; // Access the first element
+    cout<<"back element of list: "<<mylist.back()<<endl; // Access the last element
+    
+    cout<<"list is empty: "<<mylist.empty()<<endl; // Check if the list is empty
+
+    cout<<"list max size: "<<mylist.max_size()<<endl; // Maximum size of the list
+
+    cout<<"list size: "<<mylist.size()<<endl; // Size of the list
+
+    cout<<"list capacity: "<<mylist.max_size()<<endl; // Capacity of the list
+
+
+   
+    cout << endl;
+
+
+    return 0;
+}
