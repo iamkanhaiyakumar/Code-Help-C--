a83e9620860fc38b317f99c1a096c25d43b7255d
Kanhaiya Kumar <kanhaiyak0104@gmail.com>
2025-05-10 21:15:29 +0530

    stack

--- vector&STL/stl/stack.cpp
+++ vector&STL/stl/stack.cpp
@@ -30,5 +30,32 @@ int main(){
     cout << "Top element after pop: " << s.top() << endl;
     
 
+    // Create another stack
+    stack<int> first;
+    stack<int> second;
+
+    // Insert elements into the first stack
+    first.push(10);
+    first.push(20);
+    first.push(30);
+    // Insert elements into the second stack
+    second.push(40);
+    second.push(50);
+    second.push(60);
+
+    // Swap the contents of the two stacks
+
+    first.swap(second);
+    cout << "Top element of first stack after swap: " << first.top() << endl; // Access the top element of the first stack
+    cout << "Top element of second stack after swap: " << second.top() << endl; // Access the top element of the second stack   
+
+    // Clear the stack
+    while(!s.empty()){
+        s.pop(); // Remove the top element
+    }
+
+    cout << "Stack cleared." << endl;
+
+    
     return 0;
 }
