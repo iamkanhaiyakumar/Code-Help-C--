82867c5d0ac51e77c99428c26448e60ad391ddb5
Kanhaiya Kumar <kanhaiyak0104@gmail.com>
2025-05-10 21:14:01 +0530

    stack

--- vector&STL/stl/stack.cpp
+++ vector&STL/stl/stack.cpp
@@ -3,6 +3,31 @@
 using namespace std;
 
 int main(){
+
+    // Create a stack
+    stack<int> s;
+
+    //insertion
+    s.push(10);     
+    s.push(20); 
+    s.push(30);
+
+    // Access the top element
+    cout << "Top element: " << s.top() << endl;
+
+    cout<< "Size of stack: " << s.size() << endl; // Size of the stack
+
+    // Check if the stack is empty
+    if(s.empty()){
+        cout << "Stack is empty" << endl;
+    } else {
+        cout << "Stack is not empty" << endl;
+    }
+
+    // Remove the top element
+    s.pop();
+
+    cout << "Top element after pop: " << s.top() << endl;
     
 
     return 0;
