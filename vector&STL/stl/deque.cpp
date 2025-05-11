7962157777f6b9b180d1eafbf84611c88eeb1cf9
Kanhaiya Kumar <kanhaiyak0104@gmail.com>
2025-05-11 14:01:54 +0530

    deque

--- vector&STL/stl/deque.cpp
+++ vector&STL/stl/deque.cpp
@@ -17,10 +17,37 @@ int main(){
 
     d.push_back(40);
 
+    //iteration
+    cout << "Elements in deque: ";
+    deque<int>::iterator it= d.begin();
+    while(it!=d.end()){
+        cout<<*it<<" ";
+        it++;
+    }
+
+    cout << endl;
+
     // Access the front and back elements
     cout << "Front element: " << d.front() << endl;
     cout << "Back element: " << d.back() << endl;
 
+    cout << "Size of deque: " << d.size() << endl; // Size of the deque
+    //deletion
+    d.pop_front(); // Remove the front element
+    d.pop_back(); // Remove the back element
+
+    cout << "Front element after pop: " << d.front() << endl;
+
+    cout << "Back element after pop: " << d.back() << endl;
+
+    cout << "Size of deque after pop: " << d.size() << endl; // Size of the deque
+
+    // Check if the deque is empty
+    if(d.empty()){
+        cout << "Deque is empty" << endl;
+    } else {
+        cout << "Deque is not empty" << endl;
+    }
 
     return 0;
 }
