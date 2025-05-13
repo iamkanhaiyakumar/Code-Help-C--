a0a490467b55f3bf8b92aa3ef4f61ee290cc9c68
Kanhaiya Kumar <kanhaiyak0104@gmail.com>
2025-05-13 13:51:06 +0530

    algo

---
+++ vector&STL/stl_algo/min_max.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int main(){
+    //simplest way to find min and max in a vector
+    // int  a= 10;
+    // int  b= 20;
+
+    // cout<< max(a,b) << endl;
+    // cout<< min(a,b) << endl;
+
+    //min and max in a vector
+    vector<int> arr;
+    arr.push_back(10);
+    arr.push_back(20);
+    arr.push_back(30);
+    arr.push_back(40);
+    arr.push_back(50);
+
+    //min and max in a vector
+    
+
+    return 0;
+}
